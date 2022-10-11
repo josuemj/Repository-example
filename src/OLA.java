56fa25aa1455646be1a507edbc78055f4dcfb394
Jose Gramajo <gra22907@uvg.edu.gt>
2022-10-10 20:41:48 -0600

    intento2

--- src/OLA.java
+++ src/OLA.java
@@ -1,8 +1,8 @@
 public class OLA {
     public static void main(String[] args) {
         System.out.println("HEllo word!");
-        while(true){
-            System.out.println("Hello word!");
+        System.out.println("FSNDJNW");
+
         }
     }
 }
